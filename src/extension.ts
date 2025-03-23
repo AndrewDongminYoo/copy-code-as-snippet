2735356289d2bb4dd0895bc20f8e91847b884498
Dongmin Yu <ydm2790@gmail.com>
2025-03-23 12:27:15 +0900

    feat: ✨ prevent multiple activations of the extension by adding activation flag

--- src/extension.ts
+++ src/extension.ts
@@ -3,9 +3,16 @@
 import * as vscode from "vscode";
 import * as path from "path";
 
+let isActivated = false;
+
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  if (isActivated) {
+    return;
+  }
+  isActivated = true;
+
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(

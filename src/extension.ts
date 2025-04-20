6249f198b08c73ef069a232aaa501d20f7b0cdd3
Dongmin Yu <ydm2790@gmail.com>
2025-04-20 11:07:31 +0900

    fix: 🐛 remove redundant activation check
    This commit removes a redundant activation check in the `activate` function. The check was preventing the extension from being activated correctly in some cases.

--- src/extension.ts
+++ src/extension.ts
@@ -17,12 +17,6 @@ const enum SnippetFormat {
  * Registers the command and performs initialization.
  */
 export function activate(context: vscode.ExtensionContext) {
-  if (context.globalState.get("isActivated")) {
-    return;
-  }
-
-  context.globalState.update("isActivated", true);
-
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log(

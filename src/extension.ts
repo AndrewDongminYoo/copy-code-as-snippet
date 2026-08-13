62c0ab6a3804313e44323d0741f7c27fabe0713e
Dongmin, Yu <ydm2790@gmail.com>
2026-08-13 12:14:09 +0900

    chore: remove scaffold activation log

--- src/extension.ts
+++ src/extension.ts
@@ -17,12 +17,6 @@ import {
  * Registers the command and performs initialization.
  */
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "copy-code-as-snippet" is now active!',
-  );
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
